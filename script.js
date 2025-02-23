b74449bc9d6063ceb09c9e824bf5a0ae08b745ba
aklimaakter20 <aklimaakter20@gmail.com>
2025-02-24 00:12:29 +0600

    Digital Watch created

---
+++ script.js
@@ -0,0 +1,14 @@
+// function myWatch (){
+//     var dateTime = new Date();
+//     var Hours = dateTime.getHours()
+//     var Minutes = dateTime.getMinutes();
+//     var Seconds = dateTime.getSeconds();
+
+//     // var watchTime = Hours + ":" + Minutes + ":" + Seconds;
+//     document.getElementById("Hours"). innerText = Hours;
+//     document.getElementById("Minutes"). innerText = Minutes;
+//     document.getElementById("Seconds"). innerText = Seconds;
+
+// }
+// setInterval(myWatch, 1000)
+// myWatch()
